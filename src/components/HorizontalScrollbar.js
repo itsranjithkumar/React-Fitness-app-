d1ff671cd0acb19b967193bc8671e1c9b577c5bb
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-07-04 13:12:30 +0530

    bug

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,20 +1,52 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, {useContext}from "react";
+import { Box, Typography } from "@mui/material";
+import {ScrollMenu, VisibilityContext} from
+'react-horizontal-scrolling-menu';
+import 'react-horizontal-scrolling-menu/dist/styles.css'
 
-const HorizontalScrollbar = ({ data }) => {
+import BodyPart  from "./BodyPart";
+
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
   return (
-    <div>
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+
+const HorizontalScrollbar = ({ data,BodyParts,setBodyPart }) => {
+  return (
+
+    <ScrollMenu LeftArrow={LeftArrow}RightArrow={RightArrow}>
       {data.map((item) => (
         <Box
           key={item.id || item}
           itemId={item.id || item}
           title={item.id || item}
-          m="0 40px"
+          m="0px 40px"
         >
-          {item}
+
+          <BodyPart item={item} BodyPart=
+          {BodyPart}setBodyPart={setBodyPart}/>
         </Box>
       ))}
-    </div>
+    </ScrollMenu>
   );
 };
 
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,10 +1,10 @@
 export const exerciseOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    },
-  };
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+	}
+};
   
 
 export const fetchData = async (url, options) => {
