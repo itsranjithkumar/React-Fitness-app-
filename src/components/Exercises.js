6d96d97c2a53a5cca6a8d406bc7444b7fe3c1a53
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-07-08 11:16:17 +0530

    ertyu

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,6 +6,40 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 import ExerciseCard from './ExerciseCard';
 
+
+const Exercises = ({exercises,setExercises,bodyPart}) => {
+
+  const [currentPage,setCurrentPage]=useState(1);
+  const exercisesPerPage=9;
+
+  const indexOfLastExercise=currentPage*exercisesPerPage;
+  const indexOfFirstExercise=indexOfLastExercise-exercisesPerPage;
+  const currentExercises=exercises.slice(indexOfFirstExercise,indexOfLastExercise)
+
+const paginate=(e,value)=>{
+setCurrentPage(value);
+
+window.scrollTo({ top: 1800, behavior:'smooth'})
+}
+
+useEffect(()=> {
+  const fetchExerciseData = async () => {
+    let exerciseData =[]
+
+    if (bodyPart === 'all') {
+      exerciseData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions);
+    } else {
+      exerciseData = await fetchData(
+        `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+        exerciseOptions);
+    }
+  }
+
+}, [bodyPart]);
+
+
 const Exercises = ({ exercises, setExercises, bodyPart}) => {
   return (
     <Box id="exercises"
