1914eafa9295aaeb24dd0e451a1de7f5e6147360
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-07-08 16:05:02 +0530

    error

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,6 +5,17 @@ export const exerciseOptions = {
 		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
 	}
 };
+
+
+
+export const youtubeoptions = {
+	method: 'GET',
+	headers: {
+	  'x-rapidapi-key': 'e45bdb690fmsh48d6a5b21416233p1165e0jsn1b44af66a0ff',
+	  'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com'
+	}
+  };
+  
   
 
 export const fetchData = async (url, options) => {
