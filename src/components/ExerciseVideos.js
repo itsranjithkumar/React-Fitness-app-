e08eb2c402ff179ea2b606b51fede701388e829a
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-07-08 18:02:50 +0530

    2:08

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import {Box, Stack, Typography} from '@mui/material';
 const ExerciseVideos = ({exerciseVideos, name}) => {
+
+  if(!exerciseVideos.length) return 'Loading...';
   return (
     <Box sx={{marginTop:{lg:'200px',xs:'20px'}}} p="20px">
         <Typography variant="h4" mb="33px">
 
-           watch {name} exercise videos
+           watch  <span style={{color:'#ff2625', textTransform: 'capitalize'}}>{name} </span>exercise videos
         </Typography>
+        <Stack justifyContent="flex-start" flexwrap="wrap" alignItems="center"
+          sx={{
+            flexDirection: {lg : 'row'},
+            gap: {lg: '110px', xs: '0'}
+          }}
+          >
+          {exerciseVideos?.slice(0,3).map((item,index)=>(
+            <a
+             key={index}
+             className='exercise-video'
+             href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+             target='_blank'
+             rel="noreferrer"
+            >
+              <img src={item.video.thubmnails[0].url} alt={item.video.title}/>
+
+            </a>
+
+          
+          ))}
+        </Stack>
       
     </Box>
   )
